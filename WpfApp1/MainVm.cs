e0c63bc68820091748834ffade2d22f189b4a56e
shaw <1939484341@qq.com>
2023-06-28 10:52:32 +0800

    添加水印

---
+++ WpfApp1/MainVm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Windows;
+
+namespace WpfApp1
+{
+    public class MainVm
+    {
+        private static readonly Lazy<MainVm> Lazy = new Lazy<MainVm>(() => new MainVm());
+        public static MainVm Instance => Lazy.Value;
+
+        private MainVm()
+        {
+        }
+
+        /// <summary>
+        /// 添加水印
+        /// </summary>
+        /// <param name="inputImagePath"></param>
+        /// <param name="outputImagePath"></param>
+        /// <param name="watermarkText"></param>
+        public void AddWatermarkToImage(string inputImagePath, string outputImagePath, string watermarkText)
+        {
+            using Image image = Image.FromFile(inputImagePath);
+            using Bitmap bitmap = new Bitmap(image);
+            using Graphics graphics = Graphics.FromImage(bitmap);
+            graphics.SmoothingMode = SmoothingMode.AntiAlias;
+
+            using (Font font = new Font("Arial", 30, System.Drawing.FontStyle.Bold, GraphicsUnit.Pixel)) //字体
+            using (SolidBrush brush = new SolidBrush(Color.FromArgb(128, 255, 255, 255))) //颜色
+            {
+                SizeF textSize = graphics.MeasureString(watermarkText, font);
+                PointF position = new PointF(image.Width - textSize.Width - 20, image.Height - textSize.Height - 20); //水印位置
+                graphics.DrawString(watermarkText, font, brush, position);
+            }
+
+            bitmap.Save(outputImagePath, ImageFormat.Jpeg);
+        }
+
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.OpenFileDialog dialog = new System.Windows.Forms.OpenFileDialog();
+            //该值确定是否可以选择多个文件,当前不然多选
+            dialog.Multiselect = false;
+            dialog.Title = "请选择文件夹";
+            dialog.Filter = "所有文件(*.*)|*.*";
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            { 
+                var path = dialog.FileName;
+               MainVm.Instance.AddWatermarkToImage(path, "output.jpg", "十三");
+            }
+        }
+    }
+}
